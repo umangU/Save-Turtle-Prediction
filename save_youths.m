e6e5ccd1f4cb970b9be00c4a1f8378e5298d8920
umangU <72771903+umangU@users.noreply.github.com>
2021-04-23 00:20:29 +1000

    Code Updated

--- save_youths.m
+++ save_youths.m
@@ -12,12 +12,12 @@ youth (1) = 5536790;
 adult(1) = 17640;
 
 % For loop to predict calculate the future projection of each stage upto the
-% number of years entered by the user
+% Number of years entered by the user
 for i = 1:years
     hatchling(i+1) = (77.4*adult(i));
     youth(i+1) = (0.675*hatchling(i)) + (0.785*youth(i));
     adult(i+1) = (0.000847*youth(i)) + (0.809*adult(i));
 end
 
-% plotting the predicted evolution of each stages on single axis
+% Plotting the predicted evolution of each stages on single axis
 plot(0:years, hatchling, 0:years, youth,'--', 0:years, adult,':')
