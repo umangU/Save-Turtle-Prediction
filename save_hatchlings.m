017addecf403f2b7fc7f9f0c487a1eb83c89e564
umangU <72771903+umangU@users.noreply.github.com>
2021-05-23 21:08:52 +1000

    Code Updated

--- save_hatchlings.m
+++ save_hatchlings.m
@@ -1,18 +1,18 @@
-% Years taken into consideration for the projection
+%Years taken into consideration for the projection
 years = 50;
 
-% Creation of three zero row vectors based on loggerhead turtle's life stages
+%Creation of three zero row vectors based on loggerhead turtle's life stages
 hatchling = zeros(1, years+1);
 youth = zeros(1,years+1);
 adult = zeros(1, years+1);
 
-% Initialize the defined row vectors with initial values
+%Initialize the defined row vectors with initial values
 hatchling(1) = 1445570;
 youth (1) = 5536790;
 adult(1) = 17640;
 
-% For loop to predict calculate the future projection of each stage upto the
-% Number of years entered by the user
+%For loop to predict calculate the future projection of each stage upto the
+%Number of years entered by the user
 for i = 1:years
     hatchling(i+1) = (77.4*adult(i));
     youth(i+1) = (1*hatchling(i)) + (0.769*youth(i));
