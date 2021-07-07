c221e92c148ade8eee0ec69ea635f5ddd20dd1d0
umangU <72771903+umangU@users.noreply.github.com>
2021-07-07 23:36:59 +1000

    Code Updated

--- hatchlings_youth_prediction.m
+++ hatchlings_youth_prediction.m
@@ -17,8 +17,8 @@ hatchling(1) = 1445570;
 youth (1) = 5536790;
 adult(1) = 17640;
 
-%For loop to predict calculate the future projection of each stage upto the
-%Number of years entered by the user
+% For loop to predict calculate the future projection of each stage upto the
+% Number of years entered by the user
 for i = 1:years
     hatchling(i+1) = (77.4*adult(i));
     youth(i+1) = (0.675*hatchling(i)) + (0.769*youth(i));
