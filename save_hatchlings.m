a94bb8a76e92a762e045130962038bd074227b05
umangU <72771903+umangU@users.noreply.github.com>
2022-02-09 21:44:04 +1100

    Code Updated

--- save_hatchlings.m
+++ save_hatchlings.m
@@ -12,7 +12,7 @@ youth (1) = 5536790;
 adult(1) = 17640;
 
 % For loop to predict calculate the future projection of each stage upto the
-%Number of years entered by the user
+% Number of years entered by the user
 for i = 1:years
     hatchling(i+1) = (77.4*adult(i));
     youth(i+1) = (1*hatchling(i)) + (0.769*youth(i));
